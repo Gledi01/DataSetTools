3bc51f32a5dee15673653dae8bce0a1aed30c3ac
Gledi01 <162592892+Gledi01@users.noreply.github.com>
2025-08-24 01:16:50 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ cd ../
 cp -r DataSetTools ~/
 cd DataSetTools 
 mkdir main
+mkdir ./main/dataset-contoh
+cp ./contoh.dts ./main/dataset-contoh
 echo "Buka folder main menggunakan cd main, di dalamnya gunakan nano <nama datasetmu>.dts dan edit data yang ingin di simpan, nanti datanya akan di load di tools"
 echo "finish"
